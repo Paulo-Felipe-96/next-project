e378cb6973f87c8bf08c84a4c4f9d01f5e077c36
paulo.martins <paulo.martins@idwall.co>
2021-08-24 02:20:05 -0300

    Main Test Page removed

--- pages/paulo.js
+++
@@ -1,7 +0,0 @@
-import Counter from "../components/Counter";
-
-function HomePage() {
-    return <div><Counter/></div>
-}
-
-export default HomePage; // = modules.export, com default, não precisa { }
